f4d336d5020df23d6f2d6a35a8fe982018bac2cc
Neel Patel <neel.patel.2012.np@gmail.com>
2021-07-22 17:16:49 +0530

    45: Implemented notification dao layer.

---
+++ src/main/java/com/empmanagement/dao/NotificationDAO.java
@@ -0,0 +1,29 @@
+package com.empmanagement.dao;
+
+import java.util.List;
+
+import com.empmanagement.domain.Notification;
+
+public interface NotificationDAO {
+    
+    /**
+     * Create or update notification
+     * @param notification notification
+     * @return true if creation or updation is successful.
+     */
+    public boolean createOrUpdateNotification(Notification notification);
+
+    /**
+     * Get all notifications by employee id
+     * @param empId employee id
+     * @return list of notifications
+     */
+    public List<Notification> getNotifications(long empId);
+    
+    /**
+     * Get notification by notification id
+     * @param notificationId notification id
+     * @return notification
+     */
+    public Notification getNotification(int notificationId);
+}
