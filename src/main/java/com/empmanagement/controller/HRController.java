96edea89a115b0fbe8918b6f6eeb5482bce4e10c
prarthanab <pr813853@dal.ca>
2021-07-28 03:19:01 +0530

    Tests added

--- src/main/java/com/empmanagement/controller/HRController.java
+++ src/main/java/com/empmanagement/controller/HRController.java
@@ -22,150 +22,141 @@ import com.empmanagement.util.PasswordEncoder;
 
 @Controller
 public class HRController {
-	
-	@Autowired
-	private IEmpRegService empreg;
-	
-	@Autowired
-	private IReimbursementService reimburseservice;
-	
-	@Autowired
-	private IOffBoardingService offBoardService;
-	
-	@Autowired
-	private ISendEmailService sendEmail;
-	
-	private String companyEmail;
-	private String registerStatus;
-	private Long empId;
-	private double fullAndFinal;
-	private String password;
-	private String encodePass;
-	private String message;
-	private String subject;
-	private String personalEmail;
-	private int deptId;
-	static final String STATUS = "active";
-	private String empUserName;
 
-	@GetMapping("ems/hr-homescreen")
-	public String hrhomescreen(HttpSession session, Model model) {
-		Long employeeId = (Long) session.getAttribute("EMP_ID");
+    @Autowired
+    private IEmpRegService empreg;
 
-		if (employeeId == null) {
-			return "redirect:/ems/login";
-		}
-		return "hr-homescreen";
-	}
+    @Autowired
+    private IReimbursementService reimburseservice;
 
-	
-	@GetMapping("/ems/employee-registration")
-	public String empRegistrationForm(HttpSession session, Model model) {
-		Long employeeId = (Long) session.getAttribute("EMP_ID");
+    @Autowired
+    private IOffBoardingService offBoardService;
 
-		if (employeeId == null) {
-			return "redirect:/ems/login";
-		}
-		return "employee-registration";
-	}
+    @Autowired
+    private ISendEmailService sendEmail;
 
-	@PostMapping("/ems/employee-registration/submit")
-	public String regEmpDetails(@RequestParam(name = "firstname" ,required = true)String firstname,@RequestParam(name="lastname", required = true)String lastname,@RequestParam(name = "email",required = true)String email,	
-					@RequestParam(name="doj",required=true) Date doj,@RequestParam(name="dob",required=true) Date dob,@RequestParam(name="role", required=true) String role,
-					@RequestParam(name="grade" , required=true) String grade,@RequestParam(name="deptname", required =true)String deptname,
-					@RequestParam(name="team", required=true)String team,
-					RedirectAttributes redirectAttribute,
-					Model model) throws NoSuchAlgorithmException{	
-		
-		companyEmail = empreg.generateEmail(firstname, lastname);
-		deptId = empreg.getDeptId(deptname);
-		System.out.println("dept "+ deptId);
-		if(deptId == 0) {
-			redirectAttribute.addFlashAttribute("error", "Please enter a valid department");
-			return "redirect:/ems/employee-registration";
-		}
-		else {
+    private String companyEmail;
+    private String registerStatus;
+    private Long empId;
+    private double fullAndFinal;
+    private String password;
+    private String encodePass;
+    private String message;
+    private String subject;
+    private String personalEmail;
+    private int deptId;
+    static final String STATUS = "active";
+    private String empUserName;
 
-			registerStatus = empreg.registerEmp(empreg.getFullName(firstname, lastname),companyEmail,doj,dob,role,grade, deptId,team,STATUS,email);
-			empId = empreg.getEmpId(empreg.getFullName(firstname, lastname), dob);
-			System.out.println(empId);
-			password = empreg.getPassword();
-			encodePass = PasswordEncoder.encodePassword(password);
-			if(empId == 0){
-				redirectAttribute.addFlashAttribute("error", "Please try again");
-				return "redirect:/ems/employee-registration";
-			}
-			String loginDetails = empreg.updateLogin(empreg.getEmpUserName(firstname, lastname), encodePass,empId);
-			empUserName = empreg.getUsername(empId);
-			message = "Hello Welcome Aboard! Your employee Id is: " +String.valueOf(empId) + ", your username is " + empUserName+" ,password: " + password + " and company email: " + companyEmail;
-			subject = "Welcome Onboard" + firstname;
+    @GetMapping("ems/hr-homescreen")
+    public String hrhomescreen(HttpSession session, Model model) {
+        Long employeeId = (Long) session.getAttribute("EMP_ID");
 
-			if(registerStatus.equals("success") && loginDetails.equals("success")) {
+        if (employeeId == null) {
+            return "redirect:/ems/login";
+        }
+        return "hr-homescreen";
+    }
 
-				sendEmail.sendMail(subject,message, email);
-				redirectAttribute.addFlashAttribute("success", "The details of new employee have been saved successfully");
-			} else {
-				redirectAttribute.addFlashAttribute("error", "The details were not saved properly. Please try again");
-				return "redirect:/ems/employee-registration";
-			}
-		}
-		return "redirect:/ems/employee-registration";
-	}
 
-	@GetMapping("/ems/reimbursement-approval")
-	public String reimbursementapproval(HttpSession session, Model model) {
-		Long employeeId = (Long) session.getAttribute("EMP_ID");
+    @GetMapping("/ems/employee-registration")
+    public String empRegistrationForm(HttpSession session, Model model) {
+        Long employeeId = (Long) session.getAttribute("EMP_ID");
 
-		if (employeeId == null) {
-			return "redirect:/ems/login";
-		}
-		return "reimbursement-approval";
-	}
-	
-	@PostMapping("/ems/reimbursement-approval/submit")
-	public String reimburseApproval(RedirectAttributes redirectAttribute,
-			Model model) {
-		
-		String status = reimburseservice.getAllRequests();
-		if(status == "success" ) {
+        if (employeeId == null) {
+            return "redirect:/ems/login";
+        }
+        return "employee-registration";
+    }
 
-			redirectAttribute.addFlashAttribute("success", "Reimbursement approval succeeded");
-			return "redirect:/ems/reimbursement-approval";
-		}
-		else {
+    @PostMapping("/ems/employee-registration/submit")
+    public String regEmpDetails(@RequestParam(name = "firstname", required = true) String firstname, @RequestParam(name = "lastname", required = true) String lastname, @RequestParam(name = "email", required = true) String email,
+                                @RequestParam(name = "doj", required = true) Date doj, @RequestParam(name = "dob", required = true) Date dob, @RequestParam(name = "role", required = true) String role,
+                                @RequestParam(name = "grade", required = true) String grade, @RequestParam(name = "deptname", required = true) String deptname,
+                                @RequestParam(name = "team", required = true) String team,
+                                RedirectAttributes redirectAttribute,
+                                Model model) throws NoSuchAlgorithmException {
+        companyEmail = empreg.generateEmail(firstname, lastname);
+        deptId = empreg.getDeptId(deptname);
+        System.out.println("dept " + deptId);
 
-			redirectAttribute.addFlashAttribute("error", "Please try again");
-			return "redirect:/ems/reimbursement-approval";
-		}
-	}
-	
-	@GetMapping("ems/emp-offboarding")
-	public String offBoarding(HttpSession session, Model model) {
-		Long employeeId = (Long) session.getAttribute("EMP_ID");
+        if (deptId == 0) {
+            redirectAttribute.addFlashAttribute("error", "Please enter a valid department");
+            return "redirect:/ems/employee-registration";
+        } else {
+            registerStatus = empreg.registerEmp(empreg.getFullName(firstname, lastname), companyEmail, doj, dob, role, grade, deptId, team, STATUS, email);
+            empId = empreg.getEmpId(empreg.getFullName(firstname, lastname), dob);
+            System.out.println(empId);
+            password = empreg.getPassword();
+            encodePass = PasswordEncoder.encodePassword(password);
 
-		if (employeeId == null) {
-			return "redirect:/ems/login";
-		}
-		return "emp-offboarding";		
-	}
-	
-	@PostMapping("/ems/emp-offboarding/submit")
-	public String offBoardingSubmit(@RequestParam(name = "employeeId" ,required = true)Long employeeId,
-		RedirectAttributes redirectAttribute,
-		Model model){
-		
-			fullAndFinal = offBoardService.calculateFNF(employeeId);
-			message = "Hello, your FNF is " + fullAndFinal;
-			subject = "Wish you good luck!";
-			personalEmail = offBoardService.getEMail(employeeId);
-			System.out.println(personalEmail);
-			sendEmail.sendMail(subject, message,personalEmail);
-			
-			System.out.println(fullAndFinal);
-			offBoardService.disableUser(employeeId);
-			offBoardService.offBoardEmp(employeeId);
-						
-			return "redirect:/ems/emp-offboarding";	
-	}
+            if (empId == 0) {
+                redirectAttribute.addFlashAttribute("error", "Please try again");
+                return "redirect:/ems/employee-registration";
+            }
 
+            String loginDetails = empreg.updateLogin(empreg.getEmpUserName(firstname, lastname), encodePass, empId);
+            empUserName = empreg.getUsername(empId);
+            message = "Hello Welcome Aboard! Your employee Id is: " + String.valueOf(empId) + ", your username is " + empUserName + " ,password: " + password + " and company email: " + companyEmail;
+            subject = "Welcome Onboard" + firstname;
+
+            if (registerStatus.equals("success") && loginDetails.equals("success")) {
+                sendEmail.sendMail(subject, message, email);
+                redirectAttribute.addFlashAttribute("success", "The details of new employee have been saved successfully");
+            } else {
+                redirectAttribute.addFlashAttribute("error", "The details were not saved properly. Please try again");
+                return "redirect:/ems/employee-registration";
+            }
+        }
+        return "redirect:/ems/employee-registration";
+    }
+
+    @GetMapping("/ems/reimbursement-approval")
+    public String reimbursementapproval(HttpSession session, Model model) {
+        Long employeeId = (Long) session.getAttribute("EMP_ID");
+
+        if (employeeId == null) {
+            return "redirect:/ems/login";
+        }
+        return "reimbursement-approval";
+    }
+
+    @PostMapping("/ems/reimbursement-approval/submit")
+    public String reimburseApproval(RedirectAttributes redirectAttribute,
+                                    Model model) {
+        String status = reimburseservice.getAllRequests();
+
+        if (status == "success") {
+            redirectAttribute.addFlashAttribute("success", "Reimbursement approval succeeded");
+            return "redirect:/ems/reimbursement-approval";
+        } else {
+            redirectAttribute.addFlashAttribute("error", "Please try again");
+            return "redirect:/ems/reimbursement-approval";
+        }
+    }
+
+    @GetMapping("ems/emp-offboarding")
+    public String offBoarding(HttpSession session, Model model) {
+        Long employeeId = (Long) session.getAttribute("EMP_ID");
+
+        if (employeeId == null) {
+            return "redirect:/ems/login";
+        }
+        return "emp-offboarding";
+    }
+    @PostMapping("/ems/emp-offboarding/submit")
+    public String offBoardingSubmit(@RequestParam(name = "employeeId", required = true) Long employeeId,
+                                    RedirectAttributes redirectAttribute,
+                                    Model model) {
+        fullAndFinal = offBoardService.calculateFNF(employeeId);
+        message = "Hello, your FNF is " + fullAndFinal;
+        subject = "Wish you good luck!";
+        personalEmail = offBoardService.getEMail(employeeId);
+        System.out.println(personalEmail);
+        sendEmail.sendMail(subject, message, personalEmail);
+        System.out.println(fullAndFinal);
+        offBoardService.disableUser(employeeId);
+        offBoardService.offBoardEmp(employeeId);
+        return "redirect:/ems/emp-offboarding";
+    }
 }
--- src/main/java/com/empmanagement/serviceimpl/EmpRegServiceImpl.java
+++ src/main/java/com/empmanagement/serviceimpl/EmpRegServiceImpl.java
@@ -19,15 +19,19 @@ public class EmpRegServiceImpl implements IEmpRegService{
 	IEmpregisterDAO empregdao = new EmpregisterDAOImpl();
 
 	private RandomUtils randUtils = new RandomUtils();
-	
-	int empId;
+
 	String username;
+	String name;
+	String email;
+	String update;
+	int deptId;
+	Long empId;
 	static final String CAPITAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	static final String SMALL = "abcdefghijklmnopqrstuvwxyz";
     static final String NUM = "0123456789";
     static final String SPCHAR = "!@#$%^&*_=+-/.?<>)";
     static final int NUMOFCHAR = 8;
-    String update;
+
     
     @Override
 	public String registerEmp(String name, String email, Date doj, Date dob, String role, String grade, int deptId,
@@ -35,63 +39,58 @@ public class EmpRegServiceImpl implements IEmpRegService{
     	update = empregdao.registerEmp(name, email, doj, dob, role, grade, deptId, team, status, personalEmail);
 		return update;
 	}
-	   
-    
+
+	@Override
 	public Integer getDeptId(String deptname) {
-		int deptId = empregdao.getDeptId(deptname);	
+		deptId = empregdao.getDeptId(deptname);
 		return deptId;
 	}
-	
-	public String generateEmail(String firstname, String lastname) {
 
-		String email = firstname+lastname+String.valueOf(randUtils.random())+"@orgdomain.com";		
+	@Override
+	public String generateEmail(String firstname, String lastname) {
+		email = firstname+lastname+randUtils.random()+"@orgdomain.com";
 		return email;
 	}
+
+	@Override
 	public String getFullName(String firstname, String lastname) {
-		String name = firstname+" "+lastname;
+		name = firstname+" "+lastname;
 		return name;
 	}
 
+	@Override
 	public String getEmpUserName(String firstname, String lastname) {
-		
-		username = firstname + lastname + String.valueOf(randUtils.random());
+		username = firstname + lastname + randUtils.random();
 		return username;
 	}
-	
+
+	@Override
 	public String getPassword() throws NoSuchAlgorithmException {
-		
 		String combined = CAPITAL + SMALL + NUM + SPCHAR;
 		Random random = new Random();
 		char[] password = new char[NUMOFCHAR];
 		for (int i = 0; i < password.length; i++)
 	        {
 	            password[i] = combined.charAt(random.nextInt(combined.length()));
-	  
 	        }
 		return String.valueOf(password);
 	}
 
-
 	@Override
 	public Long getEmpId(String empName, Date DOB) {
-		
-		Long empId = empregdao.getEmpId(empName, DOB);
+		empId = empregdao.getEmpId(empName, DOB);
 		return empId; 
 	}
 
-
 	@Override
 	public String updateLogin(String username, String password, Long empId) {
-
-		String update = empregdao.loginDetails(username, password, empId);
+		update = empregdao.loginDetails(username, password, empId);
 		return update;
 	}
 
-
 	@Override
 	public String getUsername(Long empId) {
-		String username = empregdao.getUsername(empId);
+		username = empregdao.getUsername(empId);
 		return username;
 	}
-
 }
